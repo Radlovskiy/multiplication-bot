f228e5849b67caefabdea46245df94d1472831ab
Radlovskiy <Radlovs01@gmail.com>
2024-06-13 02:47:01 +0300

    Create multiplication_bot.py

---
+++ multiplication_bot.py
@@ -0,0 +1,5 @@
+git clone https://github.com/ваш_пользователь/telegram-multiplication-bot.git
+cd telegram-multiplication-bot
+git add multiplication_bot.py
+git commit -m "Added initial version of multiplication bot"
+git push origin main
